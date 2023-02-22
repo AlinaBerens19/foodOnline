cd855cf3ca50c2cc297fd769b748925d85891eab
alina <berens.alina019@gmail.com>
2023-02-22 15:41:42 +0200

    rub gdal only on local

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -168,6 +168,7 @@ EMAIL_USE_TLS = True
 
 GOOGLE_API_KEY = "AIzaSyA4eCZdE5ypSUoLkXs9VP56wEsa65TsFDo"
 
-os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
-os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
+if DEBUG == True:
+    os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
